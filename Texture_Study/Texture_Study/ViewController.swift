03530af261341f81f44b0cbdb0a79da5cf87107a
kimxwan0319 <kimxwan0319@naver.com>
2021-05-19 23:15:48 +0900

    ⚙️ :: Texture 사용을 위한 개발환경 세팅

--- Texture_Study/Texture_Study/ViewController.swift
+++ Texture_Study/Texture_Study/ViewController.swift
@@ -5,15 +5,17 @@
 //  Created by 김수완 on 2021/05/19.
 //
 
-import UIKit
+import AsyncDisplayKit
 
-class ViewController: UIViewController {
+class ViewController: ASDKViewController<ASDisplayNode> {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override init() {
+        super.init(node: ASDisplayNode())
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
