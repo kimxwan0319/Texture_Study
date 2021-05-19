5066d439e648edd162d784879fa0e8adf911ef2a
kimxwan0319 <kimxwan0319@naver.com>
2021-05-19 23:39:12 +0900

    📑 :: 디렉토리 구조 수정

--- Texture_Study/Texture_Study/PracticeCases/HelloTexture.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HelloTexture.swift
-//  Texture_Study
-//
-//  Created by 김수완 on 2021/05/19.
-//
-
-import Foundation
---
+++ Texture_Study/Texture_Study/PracticeCases/HelloTexture/HelloTexture.swift
@@ -0,0 +1,21 @@
+//
+//  HelloTexture.swift
+//  Texture_Study
+//
+//  Created by 김수완 on 2021/05/19.
+//
+
+import AsyncDisplayKit
+
+class HelloTextureViewController: ASDKViewController<ASDisplayNode> {
+
+    override init() {
+        super.init(node: ASDisplayNode())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
