bf9b79b494589437c62dfc40fda6f5b007e77f9b
kimxwan0319 <kimxwan0319@naver.com>
2021-05-20 15:44:54 +0900

    📑 :: [Up&Down] Practice case 추가

---
+++ Texture_Study/Texture_Study/PracticeCases/2. Up&Down/UpDownViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpDownViewController.swift
+//  Texture_Study
+//
+//  Created by 김수완 on 2021/05/20.
+//
+
+import AsyncDisplayKit
+
+class UpDownViewController: ASDKViewController<ASDisplayNode> {
+
+    override init() {
+        super.init(node: ASDisplayNode())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
